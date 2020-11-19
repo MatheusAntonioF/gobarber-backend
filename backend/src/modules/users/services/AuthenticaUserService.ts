4bffbf288717cdd38b3b20670ea380eb0dc1fd51
Matheus Antonio <matheusantoniof@gmail.com>
2020-11-19 02:01:49 -0300

    feat: create fake provider to hash password

--- backend/src/modules/users/services/AuthenticaUserService.ts
+++ backend/src/modules/users/services/AuthenticaUserService.ts
@@ -1,14 +1,12 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-
 import authConfig from '@config/auth';
-
-import User from '../infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUserRepository from '../repositories/IUserRepository';
+import User from '../infra/typeorm/entities/User';
 
 interface IRequestDTO {
   email: string;
@@ -23,7 +21,9 @@ interface IResponse {
 class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUserRepository
+    private usersRepository: IUserRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
   ) {}
 
   public async execute({ email, password }: IRequestDTO): Promise<IResponse> {
@@ -31,7 +31,10 @@ class AuthenticateUserService {
 
     if (!user) throw new AppError('Incorrect email/password combination', 401);
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password
+    );
 
     if (!passwordMatched)
       throw new AppError('Incorrect email/password combination', 401);
---
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,35 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticaUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const user = await createUser.execute({
+      name: 'Johen Doe',
+      email: 'john@example.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'john@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});
